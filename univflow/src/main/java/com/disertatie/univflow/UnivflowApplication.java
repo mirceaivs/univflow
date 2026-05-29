60c0cc88f6692f80f5c169b500b898b777b5bb52
misui <mircea.ivascu17@gmail.com>
2026-05-29 20:55:34 +0300

    adding graalVM on java backend for faster startup

--- univflow/src/main/java/com/disertatie/univflow/UnivflowApplication.java
+++ univflow/src/main/java/com/disertatie/univflow/UnivflowApplication.java
@@ -3,9 +3,12 @@ package com.disertatie.univflow;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
+import org.hibernate.dialect.PostgreSQLDialect;
 
 @SpringBootApplication
 @EnableAsync
+@RegisterReflectionForBinding(PostgreSQLDialect.class)
 public class UnivflowApplication {
 
 	public static void main(String[] args) {
